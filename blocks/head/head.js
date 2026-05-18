54b71b6eb0d46c281ed4de87f188957c93ae074b
divya-chhabra <er.divyachhabra@gmail.com>
2026-05-18 13:59:10 +0530

    feature:head_block_created

---
+++ blocks/head/head.js
@@ -0,0 +1,5 @@
+export default function decorate(block) {
+//   const children = [...block.children];
+//   if (children[0]) children[0].classList.add('head-logo');
+//   if (children[1]) children[1].classList.add('head-contact');
+}
